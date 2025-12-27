633c1535d391701c438199e1d5af586fc7f2f4ea
alya1007 <alexandra.konjevic@isa.utm.md>
2025-12-28 01:37:50 +0200

    feat(chart): add periods buttons for chart timeline

--- components/home/coin-overview.tsx
+++ components/home/coin-overview.tsx
@@ -1,28 +1,46 @@
 import { CoinDetailsData } from "@/app/page";
 import { fetcher } from "@/lib/coingecko.actions";
 import Image from "next/image";
+import CandlestickChart from "../candlestick-chart";
+
+export type OHLCData = [number, number, number, number, number];
 
 const CoinOverview = async () => {
-	let coin;
+	let coin: CoinDetailsData;
+	let coinOHLCData: OHLCData[] = [];
 	try {
-		coin = await fetcher<CoinDetailsData>("coins/bitcoin", {
-			dex_pair_format: "symbol",
-		});
+		[coin, coinOHLCData] = await Promise.all([
+			fetcher<CoinDetailsData>("coins/bitcoin", {
+				dex_pair_format: "symbol",
+			}),
+			fetcher<OHLCData[]>("coins/bitcoin/ohlc", {
+				vs_currency: "usd",
+				days: 1,
+				precision: "full",
+			}),
+		]);
 	} catch (err) {
 		console.log("Error fetching coin details:", err);
 		return <div id="coin-overview">Failed to load coin overview.</div>;
 	}
 	return (
 		<div id="coin-overview">
-			<div className="header pt-2">
-				<Image src={coin.image.large} alt={coin.name} width={40} height={40} />
-				<div className="info">
-					<p>
-						{coin.name} / {coin.symbol.toUpperCase()}
-					</p>
-					<h1>${coin.market_data.current_price.usd.toFixed(2)}</h1>
+			<CandlestickChart>
+				<div className="header pt-2">
+					<Image
+						src={coin.image.large}
+						alt={coin.name}
+						width={40}
+						height={40}
+					/>
+					<div className="info">
+						<p>
+							{coin.name} / {coin.symbol.toUpperCase()}
+						</p>
+						<h1>${coin.market_data.current_price.usd.toFixed(2)}</h1>
+					</div>
 				</div>
-			</div>
+			</CandlestickChart>
 		</div>
 	);
 };
